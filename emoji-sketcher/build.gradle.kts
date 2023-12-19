2d41fed8213fb024934927f22e32669bb38eeed5
softsuave-tech-matrix <techmatrix@softsuave.com>
2023-12-19 23:26:55 +0530

    updated the java version

--- emoji-sketcher/build.gradle.kts
+++ emoji-sketcher/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id ("maven-publish")
 }
 
 android {
     namespace = "softsuave.tech_matrix.emoji_sketcher"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
@@ -24,11 +25,24 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+}
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "softsuave-tech-matrix"
+            artifactId = "emoji-sketcher"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
     }
 }
 
